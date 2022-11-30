ac07d05c074e8fbcf484dd35c90777b40ca79bb4
Rodrigo Nunes <oliveira.rodrigonunes@gmail.com>
2022-11-30 08:33:15 -0300

    Commit inicial

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return  "Exemplo de Get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return  "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return  $"Exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return  $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
